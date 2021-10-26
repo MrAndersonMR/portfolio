772b444f36124106ca6142dc8ef801ed76ef224f
Anderson <sr.andersonmendesribeiro@gmail.com>
2021-10-26 11:53:38 -0300

    fix homepage

--- src/components/Opening/Opening.js
+++ src/components/Opening/Opening.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import Button from 'react-bootstrap/Button';
 
 const Opening = () => (
-  <div>
+  <Container>
+    <Button variant="primary">Primary</Button>{' '}
     Opening Component
-  </div>
+  </Container>
 );
 
 Opening.propTypes = {};
